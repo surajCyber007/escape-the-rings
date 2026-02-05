59b343de3cc94b9b81b3c6ecd3e28f591a003d93
Suvarna Kale <radha@Suvarnas-MacBook-Air.local>
2026-02-05 23:40:15 +0530

    modified ball speed logic

--- src/config/levels.js
+++ src/config/levels.js
@@ -1,8 +1,7 @@
 export const rings = [
-    { radius: 80, gapSize: Math.PI / 2, speed: 0.015 },
-    { radius: 100, gapSize: Math.PI / 3, speed: -0.014 },
-    { radius: 120, gapSize: Math.PI / 4, speed: 0.013 },
-    { radius: 140, gapSize: Math.PI / 5, speed: -0.012 },
-    { radius: 160, gapSize: Math.PI / 6, speed: 0.011 },
-    // { radius: 180, gapSize: Math.PI / 8, speed: -0.010 }
+    { radius: 80, gapSize: Math.PI / 2, speed: 1.2 },
+    { radius: 100, gapSize: Math.PI / 3, speed: -1.4 },
+    { radius: 120, gapSize: Math.PI / 4, speed: 1.6 },
+    { radius: 140, gapSize: Math.PI / 5, speed: -1.3 },
+    { radius: 160, gapSize: Math.PI / 6, speed: 1.1 }
 ];  
